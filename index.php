3323dc925dfd4e5a0fa52a2523fa9c5ac1c7450e
Keng_Dettal <51111337+it6239010023@users.noreply.github.com>
2020-12-10 17:01:04 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,5 +3,6 @@
   $station_name = $_GET["st"];
   $count = $_GET["c"];
   $sql = "insert into handgel ("station_name,count") values ('".$station_name."','".$count."')";
+  print $sql;
   $db -> query($sql);
 ?>
