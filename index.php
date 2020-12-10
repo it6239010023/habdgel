a05f5671247c4f72575cd6ece74dc552a7ca7dc3
Keng_Dettal <51111337+it6239010023@users.noreply.github.com>
2020-12-10 17:06:38 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 
-  ///$db = new mysqli ('localhost','6239010023','pass6239010023','6239010023');
+  $db = new mysqli ('localhost','6239010023','pass6239010023','6239010023');
   $station_name = $_GET["st"];
   $count = $_GET["c"];
-  $sql = "insert into handgel ("station_name,count") values ('".$station_name."','".$count."')";
+  $sql = "insert into handgel (station_name,count) values ('".$station_name."','".$count."')";
   print $sql;
-  //$db -> query($sql);
+  $db -> query($sql);
 ?>
