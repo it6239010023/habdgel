f419d74fd19e2c4a2c868810cc431eaf89fd06b7
Keng_Dettal <51111337+it6239010023@users.noreply.github.com>
2020-12-10 16:54:32 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+  $db = new mysqli ("localhost","6239010023","pass6239010023","6239010023");
+  $station_name = $_GET["st"];
+  $count = $_GET["c"];
+  $sql = "insert into handgel ("station_name,count") values ('".$station_name."','".$count."')";
+  $db -> query($sql);
+?>
