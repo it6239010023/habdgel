af228e100e0e6d02dd66f1190bb20859d23e402b
Keng_Dettal <51111337+it6239010023@users.noreply.github.com>
2020-12-10 17:02:57 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+  ini_set('display_errors', 1);
+  ini_set('display_startup_errors', 1);
+  error_reporting(E_ALL);
   $db = new mysqli ("localhost","6239010023","pass6239010023","6239010023");
   $station_name = $_GET["st"];
   $count = $_GET["c"];
