eab2adafcdc1d6403cdf1f5f0fbe7ccdb5a63e64
Keng_Dettal <51111337+it6239010023@users.noreply.github.com>
2020-12-28 15:14:50 +0700

    Update index.php

--- index.php
+++ index.php
@@ -169,7 +169,6 @@
     </div>
     </div>
     </body>
-
     <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
     <script src="https://cdn.datatables.net/1.10.22/js/dataTables.bootstrap5.min.js"></script>
     <script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
