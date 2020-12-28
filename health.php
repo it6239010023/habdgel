3511fe1c07b3704a61c5f430dc173d649b0c6c2d
Keng_Dettal <51111337+it6239010023@users.noreply.github.com>
2020-12-28 15:31:25 +0700

    Update health.php

--- health.php
+++ health.php
@@ -19,7 +19,7 @@
 
         //
         $strSQL = "select id, studentID, prefix, fullname, record, temp, st_health from student st inner join checkted ch on st.rfidID = ch.rfidID ORDER BY ch.id DESC LIMIT 0,5";
-	      $objQuery = $db->query($strSQL);
+        $objQuery = $db->query($strSQL) or die (mysqli_error($db));;
 	      $intNumField = mysqli_num_fields($objQuery);
 	      $resultArray = array();
 	      while($obResult = mysqli_fetch_array($objQuery))
